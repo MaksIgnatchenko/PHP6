db4b45fe04028624a87af751b22badbc90b7b9ac
Maks Ignatchenko <foecunditate@gmail.com>
2018-03-07 13:46:03 +0200

    txt_to_db

---
+++ text_to_db.php
@@ -0,0 +1,29 @@
+<?php
+$pathToFile = ""; // Path to your file you want input to db
+$tableName = "";                    // Table name you want create 
+$hn = '';                             // Your hostname 
+$db = '';                            // Your database name
+$un = '';                                // Your username to login
+$pw = '';                           // Your password 
+$handle = fopen($pathToFile, "r");
+try {
+    $pdo = new PDO("mysql:host=$hn;dbname=$db", $un, $pw);
+}
+catch (PDOException $e) {
+    echo "Connection is not completed";
+}
+$query = "create table $tableName (id int(6) not null primary key auto_increment, word varchar(100) not null default '');";
+$ver = $pdo->query($query);
+while (!feof($handle)){
+    $string = fgets($handle);
+    $stringArray = explode(" ", $string);
+    $count = count($stringArray);
+    for ($i = 0; $i < $count; $i++){
+        $word = strtolower(preg_replace("/[^A-Za-z]/", "", $stringArray[$i]));                    
+        if ($word != ""){
+            $query = "insert into $tableName (word) values ('$word')";
+            $ver = $pdo->query($query);
+        }         
+    }     
+}
+fclose($handle);
